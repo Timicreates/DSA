ef0fe42b22787d200268533f375c5e63514ee043
Timi <ktimijohn123@gmail.com>
2026-02-26 17:45:41 +0100

    new topics

---
+++ topics/reverseString.js
@@ -0,0 +1,17 @@
+const reverseString = (str) => {
+  const stack = [];
+
+  for (let char of str) {
+    stack.push(char);
+  }
+
+  let reversedString = "";
+
+  while (stack.length > 0) {
+    reversedString += stack.pop();
+  }
+  return reversedString;
+};
+
+const reversedString = reverseString("timi");
+console.log(reversedString);
